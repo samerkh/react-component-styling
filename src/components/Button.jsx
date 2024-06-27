145e789b966c093b1af7cd6b3f7e098c15b9c23b
Samer Alkhatib <khatib@cotede.ps>
2024-06-27 20:08:44 +0300

    Add tailwind to all components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -16,13 +16,13 @@
 // `;
 // export default Button;
 
-export default function Button({ ...props }) {
+export default function Button({ children, ...props }) {
   return (
     <button
       {...props}
       className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
     >
-      Click me
+      {children}
     </button>
   );
 }
